e1e99ec7cc3c8d25129e721e686f1b6ad433e2df
dev-andree <164929880+code-andre@users.noreply.github.com>
2025-08-04 20:26:23 -0300

    Ajuste do código
    Ajustado código para melhor compreensão

--- 1_Tipos de dado e operadores lógicos/2_operacao_aritmetica/script.js
+++ 1_Tipos de dado e operadores lógicos/2_operacao_aritmetica/script.js
@@ -1,36 +1,38 @@
+console.log('OPERAÇÕES ARITIMÉTICAS\n')
+
 console.log('Todos os tipos de operações originam Number\n');
 
 console.log('OPERAÇÃO DE SOMA');
 console.log(
     `Operação: 2 + 3
 Resultado: ${2 + 3}
-Tipo de: ${typeof (2 + 3)}\n`
+Tipo: ${typeof (2 + 3)}\n`
 );
 
 console.log('OPERAÇÃO DE SUBTRAÇÃO');
 console.log(
     `Operação: 5 - 12
 Resultado: ${5 - 12}
-Tipo de: ${typeof (5 - 12)}\n`
+Tipo: ${typeof (5 - 12)}\n`
 );
 
 console.log('OPERAÇÃO DE MULTIPLICAÇÃO');
 console.log(
     `Operação: 4 * 4
 Resultado: ${4 * 4}
-Tipo de: ${typeof (4 * 4)}\n`
+Tipo: ${typeof (4 * 4)}\n`
 );
 
 console.log('OPERAÇÃO DE DIVISÃO');
 console.log(
     `Operação: 5 / 3
 Resultado: ${5 / 3}
-Tipo de: ${typeof (5 / 3)}\n`
+Tipo: ${typeof (5 / 3)}\n`
 );
 
 console.log('OPERAÇÃO DE MÓDULO (RESTO DA DIVISÃO)');
 console.log(
     `Operação: 21 % 2
 Resultado: ${21 % 2}
-Tipo de: ${typeof (21 % 2)}\n`
+Tipo: ${typeof (21 % 2)}\n`
 );
